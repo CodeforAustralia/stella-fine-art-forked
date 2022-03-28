42fd5d7c23d930056b0ab25f484ec432334dbf2f
Amutha Muhunthan <tamamutha@gmail.com>
2022-03-28 12:36:02 +1100

    add lazy loader on commission image

--- src/Pages/Contact/contact.jsx
+++ src/Pages/Contact/contact.jsx
@@ -1,11 +1,9 @@
-// import Footer from '../../Components/Footer/footer';
 import ContactForm from '../../Components/Form/ContactForm';
 
 const Contact = () => {
   return (
     <div>
       <ContactForm />
-      {/* <Footer /> */}
     </div>
   );
 };
